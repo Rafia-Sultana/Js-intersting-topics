06a24fb1cbc10e05d77f9de107cb80210e26a3b2
Rafia Sultana <rafiasultana0097@gmail.com>
2022-03-01 17:18:27 +0600

    updated

--- primitive.js
+++ primitive.js
@@ -1,7 +1,22 @@
-const x = false;
+// any number positive or negative that will be truthy...
+
+
+let x = [];
+console.log('value of x', x);
 if (x) {
+    // console.log(' true value of x', x);
     console.log('variable is truthy');
 }
 else {
     console.log('variable is falsy');
 }
+
+// let y = parseInt('hello');
+// let y = [];
+// console.log('value of y', y);
+// if (x) {
+//     console.log('varaible is truthy');
+// }
+// else {
+//     console.log('variable is falsy');
+// }
---
+++ undefined-null.js
@@ -0,0 +1,21 @@
+// case-1
+let first;
+console.log(first);
+//case-2
+function second(x, y) {
+    const sum = x + y;
+
+}
+//console.log(second(7, 3));
+const result = second(7, 3);
+console.log(result);
+
+//case-3
+const fifth = {
+    name: 'sagor',
+    age: 15,
+    location: 'Dhaka'
+
+}
+// console.log(fifth.phone);
+console.log(fifth.passion);
