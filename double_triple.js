1976ed316590015c748913002e898feea729fa4d
Rafia Sultana <rafiasultana0097@gmail.com>
2022-03-01 18:03:29 +0600

    updated

---
+++ double_triple.js
@@ -0,0 +1,20 @@
+// const first = '0';
+// const second = false;
+// if (first == second) {
+//     console.log('condition is true ');
+// }
+// else {
+//     console.log('condition is false');
+// }
+
+
+
+//object and arrays are not compared by ==
+const a = [];
+const b = [];
+if (a == b) {
+    console.log('both are same');
+}
+else {
+    console.log('both are not same');
+}
